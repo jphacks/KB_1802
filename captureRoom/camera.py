d44a6ef2fcc0e5fed37992caf7ee6d5658ab0e25
Sonoda Hiroki <sonoda@mercury-2.local>
2018-10-20 15:39:09 +0900

    subprocess testcode

--- captureRoom/camera.py
+++ captureRoom/camera.py
@@ -1,13 +1,24 @@
 import time
 import picamera
+import subprocess
 
 picDir = "./"
 timeStr = time.strftime("%Y%m%d-%H%M%S")
 filename = picDir + "header" + timeStr + ".jpg"
 
+# take photo and save temp
 with picamera.PiCamera() as camera:
     camera.resolution = (128,128)
     camera.start_preview()
     time.sleep(2)
     camera.capture(filename)
+
+cmd = "ls -l"
+result = subprocess.call(cmd.split())
+if result == 0 :
+    print("send succeeded!")
+else
+    print("send failed.")
+    quit()
+
 
