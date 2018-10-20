7afd51840c444d654d93f5002d29f4a6d8cd4b93
yamanishi <yamanishi@fine.cs.kobe-u.ac.jp>
2018-10-20 14:42:20 +0900

    本番環境作り始め

--- ImageProcessing/main.py
+++ ImageProcessing/main.py
@@ -7,6 +7,73 @@ import matplotlib.pyplot as plt
 
 def main():
     # 入力画像の読み込み
+    img0 = cv2.imread("photo1.jpg")
+    img1 = cv2.imread("photo2.jpg")
+
+    res0=Quantization(img0)
+    res1=Quantization(img1)
+
+    cv2.imwrite("res0.jpg",res0)
+    cv2.imwrite("res1.jpg",res1)
+
+
+    edge0=Edgepic(res0)
+    edge1=Edgepic(res1)
+
+    print(edge1)
+
+    cv2.imwrite("res0.jpg",edge0)
+    cv2.imwrite("res1.jpg",edge1)
+
+    edgenum0=Edgenum(res0)
+    edgenum1=Edgenum(res1)
+
+    print(edgenum0)
+    print(edgenum1)
+
+    diffpic=diff(edge0,edge1)
+
+    cv2.imwrite("diff2.jpg",diffpic)
+
+
+def Quantization(img):
+    # 画像の量子化
+    Z = img.reshape((-1, 3))
+
+    # convert to np.float32
+    Z = np.float32(Z)
+
+    # define criteria, number of clusters(K) and apply kmeans()
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 8
+    ret, label, center = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+    # Now convert back into uint8, and make original image
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res2 = res.reshape((img.shape))
+    return res2
+
+def Edgepic(img):
+    #エッジを出してみる
+    edge=cv2.Canny(img,100,600)
+    return edge
+
+def Edgenum(img):
+    return np.sum(img==255)
+
+def diff(img0,img1):
+    # グレースケール変換
+    #gray0 = cv2.cvtColor(img0, cv2.COLOR_BGR2GRAY)
+    #gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+
+    # フレームの絶対差分
+    diff = cv2.absdiff(img0, img1)
+    return diff
+
+
+def main2():
+    # 入力画像の読み込み
     img0 = cv2.imread("photo0.jpg")
     img1 = cv2.imread("photo2.jpg")
 
@@ -67,6 +134,16 @@ def main():
     cv2.imwrite("diff2.jpg",diff)
     print(diff)
 
+def draw_heatmap(x, y):
+    heatmap, xedges, yedges = np.histogram2d(x, y, bins=50)
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+
+    plt.figure()
+    plt.imshow(heatmap, extent=extent)
+    plt.show()
+    plt.savefig('image.png')
 
 if __name__ == "__main__":
     main()
+    draw_heatmap([1],[1])
+
