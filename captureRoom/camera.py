349763ae1951799e38939e11d7ba39a332555fed
Sonoda Hiroki <sonoda@mercury-2.local>
2018-10-20 18:40:14 +0900

    SQL and scp

--- captureRoom/camera.py
+++ captureRoom/camera.py
@@ -1,6 +1,11 @@
 import time
 import picamera
+import pexpect
 import subprocess
+import pymysql.cursors
+
+
+#for JPHACKS 2018
 
 picDir = "./"
 timeStr = time.strftime("%Y%m%d-%H%M%S")
@@ -13,12 +18,34 @@ with picamera.PiCamera() as camera:
     time.sleep(2)
     camera.capture(filename)
 
-cmd = "ls -l"
-result = subprocess.call(cmd.split())
-if result == 0 :
-    print("send succeeded!")
-else
-    print("send failed.")
-    quit()
 
+# very danger code
+serverPath = "/home/ubuntu/camData"
+cmd = "scp " + filename + " ubuntu@52.197.145.249:" + serverPath
+scp = pexpect.spawn(cmd)
+scp.expect("Enter passphrase")
+scp.sendline("piToAWS")
+scp.interact()
+scp.close()
 
+#regsister photo to SQL
+captureTime = time.strftime("%Y-%m-%d %H:%M:%S")
+
+conn = pymysql.connect(host = '52.197.145.249',
+                        user = 'soisy',
+                        password = 'boaboa',
+                        db = 'soisy',
+                        charset = 'utf8mb4',
+                        cursorclass = pymysql.cursors.DictCursor)
+
+try :
+    with conn.cursor() as cursor :
+        sql = "INSERT INTO camData (filepath,capturedate,isClean,dirtiness) VALUES(%s,%s,%s,%s)"
+        cursor.execute(sql,(serverPath+filename,captureTime,0,0.2))
+    conn.commit()
+finally:
+    conn.close()
+
+#cleaning
+cmd = "rm " + filename
+subprocess.call(cmd.split())
